cdcd83849b5205fd566aab43263489bc80c5e9ce
Zhi Jie <zhijie12@gmail.com>
2022-07-12 21:47:08 +0800

    Added URL Shortener view

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pyshorteners
+
 
 # Create your views here.
+def shorten(request, url):
+    shorterner = pyshorteners.Shortener()
+    shortened_url = shorterner.chilpit.short(url)
+    return HttpResponse(f'Shortened URL: <a href="{shortened_url}">{shortened_url}</a>')
