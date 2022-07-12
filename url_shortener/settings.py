d662baeb6f99d8253072281bea0687fee1aa262c
Zhi Jie <zhijie12@gmail.com>
2022-07-12 23:06:31 +0800

    Added frontend app and react

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'backend.apps.BackendConfig',
     'rest_framework',
+    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('backend.urls')),
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
 ]
